414363b83aa2a2156e875da10f0856b23963d51f
“rikard189” <rikard189@gmail.com>
2020-01-20 11:36:08 -0600

    Adds partials to index

---
+++ js/index.js
@@ -0,0 +1,5 @@
+window.onload = function(){
+    $('#acciones-mejora').load('../_acciones_mejora.html');
+    $('#verificacion-eficacia').load('../_verificacion_eficacia.html');
+    $('#evaluacion-afa').load('../_evaluacion_afa.html');
+}
