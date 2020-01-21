090321c5089382f65076193a14e1e1382511a133
Ana Villarreal <ani.villarrealmtz@gmail.com>
2020-01-20 22:50:49 -0600

    adding partials to index

--- js/index.js
+++ js/index.js
@@ -2,4 +2,6 @@ window.onload = function(){
     $('#acciones-mejora').load('../_acciones_mejora.html');
     $('#verificacion-eficacia').load('../_verificacion_eficacia.html');
     $('#evaluacion-afa').load('../_evaluacion_afa.html');
+    $('#contexto-falla').load('../_contexto_falla.html');
+    $('#diagnostico-seccion').load('../_diagnostico-seccion.html');
 }
