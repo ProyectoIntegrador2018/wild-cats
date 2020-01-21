d09c2fd28f6e11bae465eb57dad1a397da5f5cbc
Ana Marcela Villarreal <ani.villarrealmtz@gmail.com>
2020-01-20 23:27:21 -0600

    Mock-up (#20)
    * initial html structure
    
    * initial html structure
    
    * adding cdn
    
    * first section mockup
    
    * making partial html
    
    * styling main page
    
    * adding partials to index
    
    * adding analisis section to index
    
    * adding section help buttons and connections with help modal

--- js/index.js
+++ js/index.js
@@ -2,4 +2,7 @@ window.onload = function(){
     $('#acciones-mejora').load('../_acciones_mejora.html');
     $('#verificacion-eficacia').load('../_verificacion_eficacia.html');
     $('#evaluacion-afa').load('../_evaluacion_afa.html');
+    $('#contexto-falla').load('../_contexto_falla.html');
+    $('#diagnostico-seccion').load('../_diagnostico-seccion.html');
+    $('#analisis-seccion').load('../_analisis_seccion.html');
 }
