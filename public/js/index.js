b503b79233d25bb0adc3fdc0deae0e9427e3956c
Ricardo Acosta <rikard189@gmail.com>
2020-01-27 09:01:10 -0600

    Adds acciones de mejora sidebar (#32)

--- public/js/index.js
+++ public/js/index.js
@@ -5,5 +5,20 @@ window.onload = function(){
     $('#contexto-falla').load('../_contexto_falla.html');
     $('#diagnostico-seccion').load('../_diagnostico-seccion.html');
     $('#analisis-seccion').load('../_analisis_seccion.html');
-    $('.show-sidebar').load('../_sidebar.html');
+    $('.show-sidebar').load('../_sidebar_acciones_mejora.html');
 }
+
+//FUNCIONALIDAD DEL SIDEBAR ACCIONES DE MEJORA
+function openNavAcciones() {
+    $("body #mySidenavAcciones").css("width", "300px");
+    $("body #mySidenavAcciones").css("padding", "4rem 2rem");
+}
+
+function closeNavAcciones() {
+    $("body #mySidenavAcciones").css("width", "0");
+    $("body #mySidenavAcciones").css("padding", "0");
+}
+
+$("#seccion-mejora .fa-question-circle").parent().click(openNavAcciones);
+$(document).on("click", "#mySidenavAcciones .closebtn", closeNavAcciones);
+
